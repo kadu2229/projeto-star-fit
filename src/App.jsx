ab77f55cc02ad24f594c31630d1764c40c8ccd4b
Carlos <kadu2229@yahoo.com.br>
2025-10-21 21:33:30 -0300

    bug da ide

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './components/header'
-import Hero from './components/Hero'
-import Planos from './components/Planos'
-import Mapa from './components/Mapa'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Planos from './components/Planos';
+import Mapa from './components/Mapa';
 
 function App() {
   return (
